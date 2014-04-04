c32bff1d36b4b87fa5e85d305ccc02b0d513f8fd
abeham <abeham@heuristiclab.com>
2014-04-04 13:20:11 +0200

    Changed version to 3.0.3 to match that of SimPy.

--- SimSharp/Properties/AssemblyInfo.cs
+++ SimSharp/Properties/AssemblyInfo.cs
@@ -23,7 +23,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SimSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET port of SimPy, discrete event simulation framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HEAL")]
 [assembly: AssemblyProduct("SimSharp")]
@@ -49,5 +49,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.3.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]

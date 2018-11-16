db53aaadb0b8dd9c04a2cd464aefad74f44498ce
Andreas Beham <andreas.beham@heuristiclab.com>
2018-11-16 13:59:27 +0100

    Default new Simulation base class to Pcg random number generator and leave old Environment to System.Random

---
+++ SimSharp/Random/PcgRandom.cs
@@ -0,0 +1,32 @@
+﻿namespace SimSharp {
+  public class PcgRandom : IRandom {
+    private Pcg pcg;
+
+    public PcgRandom() {
+      pcg = new Pcg();
+    }
+
+    public PcgRandom(int seed) {
+      pcg = new Pcg(seed);
+    }
+    public int Next() {
+      return pcg.Next();
+    }
+
+    public int Next(int upperBound) {
+      return pcg.Next(upperBound);
+    }
+
+    public int Next(int lowerBound, int upperBound) {
+      return pcg.Next(lowerBound, upperBound);
+    }
+
+    public double NextDouble() {
+      return pcg.NextDouble();
+    }
+
+    public void Reinitialise(int seed) {
+      pcg = new Pcg(seed);
+    }
+  }
+}

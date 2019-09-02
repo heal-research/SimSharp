f12ad773cf8d95dd901f4e7ed1526c0602bb1136
Andreas Beham <andreas.beham@heuristiclab.com>
2019-09-02 10:22:55 +0200

    Change license header of source files

--- src/Benchmark/Program.cs
+++ src/Benchmark/Program.cs
@@ -1,19 +1,8 @@
 ﻿#region License Information
-/* SimSharp - A .NET port of SimPy, discrete event simulation framework
-Copyright (C) 2019  Heuristic and Evolutionary Algorithms Laboratory (HEAL)
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/*
+ * This file is part of SimSharp which is licensed under the MIT license.
+ * See the LICENSE file in the project root for more information.
+ */
 #endregion
 
 using CommandLine;
